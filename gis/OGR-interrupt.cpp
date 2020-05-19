c551c89a606115238706b6d4dff1b148e83ea237
Mika Heiskanen <mika.heiskanen@fmi.fi>
2020-05-19 03:26:25 +0000

    Split projection interrupts into smaller segments to enabled curved cuts

--- gis/OGR-interrupt.cpp
+++ gis/OGR-interrupt.cpp
@@ -8,7 +8,7 @@ namespace Fmi
 namespace OGR
 {
 const double epsilon = 1e-6;
-const double poleshift = 90.1;  // extend the cut beyond the poles for boolean operations
+const double poleshift = 0.1;  // extend the cut beyond the poles for boolean operations
 
 double modlon(double lon)
 {
@@ -17,14 +17,25 @@ double modlon(double lon)
   return lon;
 }
 
-OGRPolygon* vertical_cut(double lon, double lat1, double lat2)
+// Add latitudes at 1 degree intervals as in -90.1 -89 -88 ... 89 90.1
+
+OGRPolygon* vertical_cut(double lon, int lat1, int lat2)
 {
   OGRPolygon* poly = new OGRPolygon;
   OGRLinearRing* ring = new OGRLinearRing;
-  ring->addPoint(lon - epsilon, std::min(lat1, lat2));
-  ring->addPoint(lon - epsilon, std::max(lat1, lat2));
-  ring->addPoint(lon + epsilon, std::max(lat1, lat2));
-  ring->addPoint(lon + epsilon, std::min(lat1, lat2));
+  auto x1 = lon - epsilon;
+  auto x2 = lon + epsilon;
+  auto y1 = std::min(lat1, lat2);
+  auto y2 = std::max(lat1, lat2);
+
+  // From lon-eps,y1 up to to lon-eps,y2
+  for (int y = y1; y <= y2; ++y)
+    ring->addPoint(x1, y + (y == -90 ? -poleshift : y == 90 ? +poleshift : 0));
+
+  // From lon+eps,y2 down to lon+eps,y1
+  for (int y = y2; y >= y1; --y)
+    ring->addPoint(xx, y + (y == -90 ? -poleshift : y == 90 ? +poleshift : 0));
+
   ring->closeRings();
   poly->addRingDirectly(ring);
   return poly;
@@ -45,8 +56,8 @@ OGRMultiPolygon* interruptGeometry(const OGRSpatialReference& theSRS)
     const double lon_wrap = theSRS.GetNormProjParm("TODO", 180.0);
     OGRMultiPolygon* mpoly = new OGRMultiPolygon;
     mpoly->assignSpatialReference(OGRSpatialReference::GetWGS84SRS());
-    mpoly->addGeometryDirectly(vertical_cut(modlon(lon_wrap - 180), -poleshift, poleshift));
-    mpoly->addGeometryDirectly(vertical_cut(modlon(lon_wrap + 180), -poleshift, poleshift));
+    mpoly->addGeometryDirectly(vertical_cut(modlon(lon_wrap - 180), -90, 90));
+    mpoly->addGeometryDirectly(vertical_cut(modlon(lon_wrap + 180), -90, 90));
     return mpoly;
   }
 
@@ -64,29 +75,29 @@ OGRMultiPolygon* interruptGeometry(const OGRSpatialReference& theSRS)
   if (name == SRS_PT_ORTHOGRAPHIC) return nullptr;
   if (name == SRS_PT_TWO_POINT_EQUIDISTANT) return nullptr;
 
-  // lon_0 is now needed for all geometric projections
+  // lon_0 is needed for all remaining geometric projections
 
   const double lon_0 = theSRS.GetNormProjParm(SRS_PP_CENTRAL_MERIDIAN, 0.0);
 
   if (name == SRS_PT_IGH)
   {
-    // Regular land cut Goode homolosine, no other variants are supported in GDAL30/PROJ.7
+    // Interrupted Goode Homolosine
     OGRMultiPolygon* mpoly = new OGRMultiPolygon;
     mpoly->assignSpatialReference(OGRSpatialReference::GetWGS84SRS());
-    mpoly->addGeometryDirectly(vertical_cut(modlon(lon_0 - 180), -poleshift, poleshift));
-    mpoly->addGeometryDirectly(vertical_cut(modlon(lon_0 + 180), -poleshift, poleshift));
-    mpoly->addGeometryDirectly(vertical_cut(modlon(lon_0 - 40), 0, poleshift));
-    mpoly->addGeometryDirectly(vertical_cut(modlon(lon_0 - 100), -poleshift, 0));
-    mpoly->addGeometryDirectly(vertical_cut(modlon(lon_0 - 20), -poleshift, 0));
-    mpoly->addGeometryDirectly(vertical_cut(modlon(lon_0 + 80), -poleshift, 0));
+    mpoly->addGeometryDirectly(vertical_cut(modlon(lon_0 - 180), -90, 90));
+    mpoly->addGeometryDirectly(vertical_cut(modlon(lon_0 + 180), -90, 90));
+    mpoly->addGeometryDirectly(vertical_cut(modlon(lon_0 - 40), 0, 90));
+    mpoly->addGeometryDirectly(vertical_cut(modlon(lon_0 - 100), -90, 0));
+    mpoly->addGeometryDirectly(vertical_cut(modlon(lon_0 - 20), -90, 0));
+    mpoly->addGeometryDirectly(vertical_cut(modlon(lon_0 + 80), -90, 0));
     return mpoly;
   }
 
   // Regular geometric: cut everything at lon_0+180 antimeridian
   OGRMultiPolygon* mpoly = new OGRMultiPolygon;
   mpoly->assignSpatialReference(OGRSpatialReference::GetWGS84SRS());
-  mpoly->addGeometryDirectly(vertical_cut(modlon(lon_0 - 180), -poleshift, poleshift));
-  mpoly->addGeometryDirectly(vertical_cut(modlon(lon_0 + 180), -poleshift, poleshift));
+  mpoly->addGeometryDirectly(vertical_cut(modlon(lon_0 - 180), -90, 90));
+  mpoly->addGeometryDirectly(vertical_cut(modlon(lon_0 + 180), -90, 90));
   return mpoly;
 }
 
